408308e89f8ad130489e5a475ff2c1dd0a479092
ujjwaldev4 <uy2027028@gmail.com>
2024-09-20 12:13:03 +0530

    Add files via upload

---
+++ FLOAT.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ clrscr();
+ float first=22.33;
+ float second=21.2;
+ printf("%1f \n",first*second);
+ printf("%2f",first*second);
+ getch();
+}
