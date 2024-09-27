1391ffe0fb8cc562813671fc9e920f210f9a549c
ujjwaldev4 <uy2027028@gmail.com>
2024-09-27 12:34:24 +0530

    Add files via upload

---
+++ largest.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+ {
+ 	int n;
+ 	printf("enter a value :");
+ 	scanf("%d",&n);
+ 	if(n%2==0){
+ 		printf("even no");
+	 }
+ 	else{
+ 		printf("odd no");
+	 }
+ return 0;
+ }
