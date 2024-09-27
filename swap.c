ec1594eb1aa6ea6de5c79fe2349e69b58b44b40a
ujjwaldev4 <uy2027028@gmail.com>
2024-09-27 12:21:31 +0530

    Add files via upload

---
+++ swap.c
@@ -0,0 +1,14 @@
+#include<studio.h>
+int main()
+ {
+ 	int a,b,c;
+ 	printf("enter the value a \n");
+ 	scanf("%d",&a);
+ 	printf("enter the value b \n");
+ 	scanf("%d",&b);
+ 	c=a;
+ 	a=b;
+ 	b=c;
+ 	printf("enter the value %d and %d",a,b);
+ return 0;
+ }
